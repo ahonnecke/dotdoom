66f5096f0a0aa0476ed93996cd6472c421476c69
Ashton Honnecke <ashton@pixelstub.com>
2023-11-17 12:05:42 -0700

    After rebuild at 29.1

--- custom.el
+++ custom.el
@@ -3,10 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ignored-local-variable-values
+   '((vc-prepare-patches-separately)
+     (diff-add-log-use-relative-names . t)
+     (vc-git-annotate-switches . "-w")))
  '(org-safe-remote-resources
    '("\\`file:///ssh:ahonnecke@honnecke\\.us:/var/www/honnecke/beampockets/market_research\\.org\\'"))
  '(package-selected-packages
-   '(flymake-ruff python-coverage python-pytest ox-gist ox-html5slide ox-jira ox-json ox-report docker-tramp)))
+   '(flymake-ruff python-coverage python-pytest ox-gist ox-html5slide ox-jira ox-json ox-report)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- packages.el
+++ packages.el
@@ -49,7 +49,6 @@
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
 
-(package! docker-tramp)
 (package! magit-delta)
 (package! ob-graphql)
 (package! direnv)
