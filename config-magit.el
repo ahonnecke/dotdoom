bc1b04a2938172d7622f687a113b1f99832fb52b
Ashton Honnecke <ashton@pixelstub.com>
2024-10-18 08:33:34 -0600

    add igist

--- config-magit.el
+++ config-magit.el
@@ -1,4 +1,4 @@
-(global-set-key (kbd "M-m") 'magit-status)
+(global-set-key (kbd "C-c m") 'magit-status)
 
 (with-eval-after-load "magit" (define-key magit-process-mode-map (kbd "<return>") 'find-file-at-point-with-line))
 
@@ -6,7 +6,6 @@
 
 (with-eval-after-load "magit"
   (define-key magit-mode-map (kbd "C-c n") 'forge-create-pullreq)
-  (define-key magit-mode-map (kbd "C-c m") 'forge-merge)
   (define-key magit-mode-map (kbd "C-c w") 'forge-browse-dwim)
   (define-key magit-mode-map (kbd "C-c a") 'magit-abort-dwim)
   (define-key magit-mode-map [tab] 'magit-section-toggle)
